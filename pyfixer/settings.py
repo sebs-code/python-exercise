464fa8695be492d00f7b49be3daece910aef5781
Sebastian David Lees <sebastian@incerto.net>
2024-01-16 21:43:07 +0000

    Update settings.py

--- pyfixer/settings.py
+++ pyfixer/settings.py
@@ -2,7 +2,7 @@
 # Settings file for the pyfixer package
 
 API_HOME = 'http://data.fixer.io/api/'
-API_KEY = '75d128d5e0997c42cfae0e6d110eeaf9'
+API_KEY = 'XXXXXXXX'
 BASE_CURRENCY = 'EUR'
 CURRENCIES = ['GBP', 'USD']
 DB_URI = 'sqlite:///db.sqlite3'
